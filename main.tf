f6c7e0ba1e917d5945ea0caf845dc2b4f0f4d6f4
Dave Wakeman <dwakeman@us.ibm.com>
2020-05-25 20:12:55 -0500

    removed cluster and api key ref

--- main.tf
+++ main.tf
@@ -74,6 +74,7 @@ module "app_subnets" {
 
 }
 
+/*
 ##############################################################################
 # Create OpenShift Cluster
 ##############################################################################
@@ -100,7 +101,7 @@ resource "ibm_container_vpc_cluster" "app_cluster" {
         name      = "${var.region}-3"
     }
 }
-
+*/
 
 output vpc_id {
  value = module.vpc.vpc_id
